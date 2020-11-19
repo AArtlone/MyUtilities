0420305a6337fb6cbe02501f4cb3eb4546a8c7d9
AArtlone <artem.stolyga@gmail.com>
2020-11-19 20:15:29 +0100

    Added package files to the root of repo

---
+++ Runtime/GUI/Effects/EffectScriptableObject/FloatEffectSO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyUtilities.GUI
+{
+    [CreateAssetMenu(fileName = "floatEffectSO", menuName = "FloatEffectSO")]
+    public class FloatEffectSO : EffectSOBase
+    {
+        public float startValue;
+        public float targetValue;
+    }
+}
---
+++ Runtime/GUI/Effects/Tween.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using UnityEngine;
+
+namespace MyUtilities.GUI
+{
+    [Serializable]
+    public class Tween
+    {
+        public TweenPlayStyle playStyle;
+        public AnimationCurve curve;
+        public float targetTime;
+        public float delay;
+
+        public bool NeedsDelay { get { return delay != 0; } }
+    }
+}
