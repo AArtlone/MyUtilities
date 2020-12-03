b8142fa8a07e9e4ea8625adb3cd482f0777aad78
AArtlone <artem.stolyga@gmail.com>
2020-12-03 21:03:37 +0100

    Created Tooltip functionality

---
+++ Runtime/GUI/Tooltip/MyTooltip.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public static class MyTooltip
+{
+    private static Tooltip tooltipOnScreen;
+
+    public static void CreateTooltip(Vector2 position, List<string> contentElements, string title = "")
+    {
+        var tooltipContainer = GameObject.FindGameObjectWithTag("TooltipContainer");
+
+        if (tooltipContainer == null)
+        {
+            Debug.LogError("TooltipContainer could not been found in the scene. Assing the TooltipContainer tag the container object in the scene");
+            return;
+        }
+
+        tooltipOnScreen = Object.Instantiate(Resources.Load<Tooltip>("Prefabs/TooltipPrefab"), tooltipContainer.transform);
+
+        tooltipOnScreen.SePosition(position);
+        tooltipOnScreen.SetTitle(title);
+        tooltipOnScreen.SetContent(contentElements);
+    }
+
+    public static void DestroyTooltip()
+    {
+        if (tooltipOnScreen == null)
+        {
+            Debug.LogWarning("Tooltip is null");
+            return;
+        }
+
+        Object.Destroy(tooltipOnScreen.gameObject);
+    }
+}
---
+++ Runtime/GUI/Tooltip/Tooltip.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RectTransform))]
+public class Tooltip : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI title = default;
+
+    [SerializeField] private LayoutElement layoutElement = default;
+
+    [SerializeField] private TextMeshProUGUI contentPrefab = default;
+
+    [SerializeField] private int characterWrapLimit = default;
+
+    private RectTransform rectTransform;
+
+    private void Awake()
+    {
+        rectTransform = GetComponent<RectTransform>();
+    }
+
+    private void Update()
+    {
+        if (Application.isEditor && Input.GetMouseButtonDown(0))
+            Destroy(gameObject);
+
+        if (Input.touchCount > 0)
+            Destroy(gameObject);
+    }
+
+    public void SePosition(Vector2 position)
+    {
+        float pivotX = position.x / Screen.width;
+        float pivotY = position.y / Screen.height;
+
+        rectTransform.pivot = new Vector2(pivotX, pivotY);
+        transform.position = position;
+    }
+
+    public void SetTitle(string title)
+    {
+        if (string.IsNullOrEmpty(title))
+        {
+            this.title.gameObject.SetActive(false);
+            return;
+        }
+
+        this.title.text = title;
+
+        int headerLength = this.title.text.Length;
+
+        layoutElement.enabled = (headerLength > characterWrapLimit) ? true : false;
+    }
+
+    public void SetContent(List<string> contentElements)
+    {
+        for (int i = 0; i < contentElements.Count; i++)
+        {
+            var obj = Instantiate(contentPrefab, transform);
+            obj.text = contentElements[i];
+
+            int contentLength = contentElements[i].Length;
+            layoutElement.enabled = (contentLength > characterWrapLimit) ? true : false;
+        }
+    }
+}
