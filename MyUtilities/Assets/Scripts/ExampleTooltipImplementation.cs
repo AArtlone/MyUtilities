5c75368b33c2810e1058d6ec4982e6003e4f8d32
AArtlone <artem.stolyga@gmail.com>
2020-12-03 21:41:22 +0100

    Fixed a bug in Tooltip and added an example scene (not in the package example)

---
+++ MyUtilities/Assets/Scripts/ExampleTooltipImplementation.cs
@@ -0,0 +1,20 @@
+﻿using MyUtilities.GUI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleTooltipImplementation : MonoBehaviour
+{
+    private MyButton myButton;
+
+    private void Awake()
+    {
+        myButton = GetComponent<MyButton>();
+        myButton.onClick += Btn_OnClick;
+    }
+
+    private void Btn_OnClick()
+    {
+        var contentElemenets = new List<string> { "aa", "bbb" };
+        MyTooltip.CreateTooltip(transform.position, contentElemenets);
+    }
+}
