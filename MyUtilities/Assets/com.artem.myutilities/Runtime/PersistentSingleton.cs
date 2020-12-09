e95acff01718aad7ad8397c7ede0a5d027f2c011
AArtlone <artem.stolyga@gmail.com>
2020-12-09 20:16:26 +0100

    Added PersistentSingleton

---
+++ MyUtilities/Assets/com.artem.myutilities/Runtime/PersistentSingleton.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace MyUtilities
+{
+    public abstract class PersistentSingleton<T> : MonoBehaviour where T : PersistentSingleton<T>
+    {
+        public static T Instance;
+
+        public static bool Exists { get; private set; }
+
+        protected abstract void Awake();
+
+        protected void SetInstance(T instance)
+        {
+            if (Instance !=  null)
+            {
+                Destroy(this);
+                return;
+            }
+
+            Instance = instance;
+            Exists = true;
+            DontDestroyOnLoad(this);
+        }
+
+        private void OnDestroy()
+        {
+            if (Instance == this)
+            {
+                Instance = null;
+                Exists = false;
+            }
+        }
+    }
+}
