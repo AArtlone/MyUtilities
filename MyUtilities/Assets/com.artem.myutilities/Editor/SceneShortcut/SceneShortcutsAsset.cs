f77e874aaaab963f04512422247f5e50a6651e0c
AArtlone <artem.stolyga@gmail.com>
2020-12-14 21:05:28 +0100

    Created SceneShortcut window

---
+++ MyUtilities/Assets/com.artem.myutilities/Editor/SceneShortcut/SceneShortcutsAsset.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewSettingsAsset", menuName = "SceneShortcuts")]
+public class SceneShortcutsAsset : ScriptableObject
+{
+    public List<SceneShortcut> sceneShortcuts = default;
+}
+
+[System.Serializable]
+public class SceneShortcut
+{
+    public string name;
+    public SceneAsset sceneAsset;
+}
+
