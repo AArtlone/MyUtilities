fd8ad5801c71bb1c1fd5f73905546701ce1f5233
AArtlone <artem.stolyga@gmail.com>
2021-01-05 17:05:58 +0100

    Added an Example for the SelectableController

---
+++ Samples/SelectableController/ExampleSelectableCell.cs
@@ -0,0 +1,23 @@
+﻿using MyUtilities.GUI;
+using TMPro;
+using UnityEngine;
+
+public class ExampleSelectableCell : SelectableCell<ExampleSelectableCellData>
+{
+    [SerializeField] private TextMeshProUGUI textComponent = default;
+
+    public override void Refresh()
+    {
+        textComponent.text = data.someText;
+    }
+}
+
+public class ExampleSelectableCellData : SelectableCellData
+{
+    public string someText;
+
+    public ExampleSelectableCellData(string text)
+    {
+        someText = text;
+    }
+}
