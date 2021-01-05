76974922f37eb3d49108e1449f48a49585f6e9d6
AArtlone <artem.stolyga@gmail.com>
2021-01-05 17:05:58 +0100

    Added an Example for the SelectableController

---
+++ MyUtilities/Assets/com.artem.myutilities/Samples/SelectableController/ExampleSelectableController.cs
@@ -0,0 +1,24 @@
+﻿using MyUtilities.GUI;
+using System.Collections.Generic;
+
+public class ExampleSelectableController : SelectableController<ExampleSelectableCell, ExampleSelectableCellData>
+{
+    private void Awake()
+    {
+        List<ExampleSelectableCellData> dataSet = new List<ExampleSelectableCellData>();
+
+        for (int i = 0; i < 3; i++)
+        {
+            dataSet.Add(new ExampleSelectableCellData(i.ToString()));
+        }
+
+        SetDataSet(dataSet);
+    }
+
+    protected override void Cell_OnCellPress(SelectableCell<ExampleSelectableCellData> cell)
+    {
+        base.Cell_OnCellPress(cell);
+
+        print(cell.data.someText);
+    }
+}
