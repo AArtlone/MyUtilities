7447bd643be312a3a1213c8933adc75fcd0d4c60
AArtlone <artem.stolyga@gmail.com>
2021-10-17 15:48:17 +0200

    Tooltip now works with mouse hover instead of click
    Placed tooltip related files in the correct samples folder

---
+++ Samples/Tooltip/ExampleTooltipImplementation.cs
@@ -0,0 +1,34 @@
+﻿using MyUtilities.GUI;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExampleTooltipImplementation : MonoBehaviour
+{
+    [SerializeField] private List<string> textToShow = default;
+
+    private MyButton myButton;
+
+    private void Awake()
+    {
+        myButton = GetComponent<MyButton>();
+
+        myButton.onPointerEnter += Btn_OnPointerEnter;
+        myButton.onPointerExit += Btn_OnPointerExit;
+    }
+
+    private void OnDestroy()
+    {
+        myButton.onPointerEnter -= Btn_OnPointerEnter;
+        myButton.onPointerExit -= Btn_OnPointerExit;
+    }
+
+    private void Btn_OnPointerEnter()
+    {
+        MyTooltip.CreateTooltip(transform.position, textToShow);
+    }
+
+    private void Btn_OnPointerExit()
+    {
+        MyTooltip.DestroyTooltip();
+    }
+}
