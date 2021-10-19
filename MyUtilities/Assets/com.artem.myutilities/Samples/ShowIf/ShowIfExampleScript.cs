8bc6d765331dc7640471bede444340695d53bdf2
AArtlone <artem.stolyga@gmail.com>
2021-10-19 18:52:27 +0200

    Added Sample for the ViewController and ShowIf

---
+++ MyUtilities/Assets/com.artem.myutilities/Samples/ShowIf/ShowIfExampleScript.cs
@@ -0,0 +1,15 @@
+﻿using MyUtilities;
+using UnityEngine;
+
+public class ShowIfExampleScript : MonoBehaviour
+{
+    [SerializeField] private bool showInt = default;
+
+    [ShowIf(nameof(showInt), true, ComparisonType.Equals)]
+    [SerializeField] private int someInt = default;
+
+    [Header("Set this variable to 1 to see the effect of the ShowIf attribute")]
+    [SerializeField] private int showBool = default;
+    [ShowIf(nameof(showBool), 1, ComparisonType.Equals)]
+    [SerializeField] private bool someBool = default;
+}
